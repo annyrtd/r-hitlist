eefcaf05f5fff90578f0365e9ba6ff16c6c9f299
jahglow <jahglow@gmail.com>
2016-11-29 10:35:45 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Hitlist from "./hitlist";
-import ReportalBase from "r-reporal-base/src/reportal-base"
+import ReportalBase from "r-reportal-base"
 
 window.Reportal = window.Reportal || {};
 ReportalBase.mixin(window.Reportal,{
