acbb3ba366c03320f73c7b47bb6f5703e356fe62
annyrtd <fyz95@mail.ru>
2017-03-09 13:52:54 +0300

    hitlist different types of chips - add ellipsis - new case with hover effect

--- src/hitlist.js
+++ src/hitlist.js
@@ -400,14 +400,14 @@ class Hitlist {
 
     categoryDiv.onmouseover = () => {
       categoryDots.style.width = '0px';
-      categoryDots.style.overflow = 'hidden';
+      categoryDots.style.display = 'none';
       categoryCardOuter.style.width = (mainCategoryCard.clientWidth + 8) + 'px';
     };
 
     categoryDiv.onmouseout = () => {
       categoryDots.style.width = '12px';
       categoryCardOuter.style.width = '12px';
-      categoryDots.style.overflow = '';
+      categoryDots.style.display = '';
     };
 
     return categoryDiv
